db20c31e0fab6dd8ccc7693dd59978cb8e27fd64
Chad van Basedman <113964826+redrededredred@users.noreply.github.com>
2023-04-11 16:18:40 +0200

    Fix main.js
    getCookie was not awaited in main function

--- main.js
+++ main.js
@@ -1,39 +1,44 @@
-// function to get a valid cookie to use the api
-
-// TODO: add getCookie function
+// function to get a valid cookie to use with the api
 async function getValidCookie() {
+
   const url = "https://www.vinted.de/"
-  var response = (await fetch(url))
-  var newCookie = ""
-  for (i = 0; i < response.headers.get("Set-Cookie").split(" ").length; i++) {
-    if(response.headers.get("Set-Cookie").split(" ")[i].startsWith("_vinted_fr_session")) {
-      newCookie = response.headers.get("Set-Cookie").split(" ")[i]
-      break
+  var response = await fetch(url)
+  var allCookies = response.headers.get("Set-Cookie").split(" ")
+
+  for (i = 0; i < allCookies.length; i++) {
+    
+    if(allCookies[i].startsWith("_vinted_fr_session")) {
+      return allCookies[i]
     }
   }
-  return newCookie
 }
 
-// main function to scrape vinted for a given item
-
+// main function to scrape vinted for a given item 
 async function getVintedItems(query) {
+  // TODO: return the json instead of logging it for later use
   var cookie = await getValidCookie()
   var current_page = 1
-  var response = await fetch(`https://www.vinted.de/api/v2/catalog/items?page=${current_page.toString}&per_page=500&search_text=${query}`, {method: "GET", 
+  var url = `https://www.vinted.de/api/v2/catalog/items?page=${current_page.toString}&per_page=500&search_text=${query}`
+
+  // TODO: add do while loop? this part looks stupid
+  var response = await fetch(url, {method: "GET", 
   headers: {
     "Cookie": cookie
   }})
+
   var jsonData = await response.json();
   console.log(jsonData);
 
   current_page = current_page + 1
 
   while (jsonData["pagination"]["current_page"] != jsonData["pagination"]["total_pages"]) {
-    var response = await fetch(`https://www.vinted.de/api/v2/catalog/items?page=${current_page.toString}&per_page=500&search_text=${query}`, {method: "GET", 
+    var response = await fetch(url, {method: "GET", 
   headers: {
     "Cookie": cookie
   }})
+
   var jsonData = await response.json();
+
   console.log(jsonData);
   current_page = current_page + 1
   console.log(current_page)
@@ -41,4 +46,4 @@ async function getVintedItems(query) {
   };
 }
 
-getVintedItems("lol")
+getVintedItems("awd")
